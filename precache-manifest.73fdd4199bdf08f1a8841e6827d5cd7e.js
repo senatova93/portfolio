baa6f864b38bb41e7b5fc6b267d7d52452810461
Senatova93 <nanniin160@gmail.com>
2020-03-29 12:43:05 +0300

    Updates

---
+++ precache-manifest.73fdd4199bdf08f1a8841e6827d5cd7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f390c04b37d6b05fce042e38c07ec00",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6b0bad7530291968cb24",
+    "url": "/portfolio/static/css/main.aa816827.chunk.css"
+  },
+  {
+    "revision": "1630136541992d7534c9",
+    "url": "/portfolio/static/js/2.12ddb7d7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.12ddb7d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b0bad7530291968cb24",
+    "url": "/portfolio/static/js/main.4f0e119b.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
