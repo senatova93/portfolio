a6c8d08b04bb18eab873369b52276e0d16074eb1
Senatova93 <nanniin160@gmail.com>
2020-04-21 14:20:39 +0300

    first step

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,16 +4,10 @@ import styles from './Footer.module.css';
 function Footer() {
     return (
         <div className={styles.footer}>
-            <div className={styles.container}>
-           <span className={styles.blockTitle}>Анастасия Сенатова </span>
-            <div className={styles.socialBlock}>
-                <div className={styles.socialIcon}></div>
-                <div className={styles.socialIcon}></div>
-                <div className={styles.socialIcon}></div>
-                <div className={styles.socialIcon}></div>
-            </div>
+
+
                 <span className={styles.copyRight}>© 2020, All Rights Reserved</span>
-            </div>
+
         </div>
     );
 }
