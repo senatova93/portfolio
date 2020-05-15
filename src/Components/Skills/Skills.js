cea77ed04a0192663c88823b5120059427d60305
Senatova93 <nanniin160@gmail.com>
2020-05-15 08:03:57 +0300

    step2

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -2,36 +2,37 @@ import React from 'react';
 import styles from './Skills.module.css';
 
 function Skills() {
-  return (<div className={styles.container}>
+    return (<div className={styles.container}>
 
-          <div className={styles.skills}>
-              <h3>My Skills</h3>
+            <div className={styles.skills}>
+                <div className={styles.blockTitle}>
+                <h2 className={styles.blockTitle}>My Skills</h2>
+                <div className={styles.line}></div>
+                </div>
+                <div className={styles.skillBlock}>
 
-              <div className={styles.skillBlock}>
-
-                  <div className={styles.skill}>
-                      <h4>My skill</h4>
-                      <h5>discription skill</h5>
-                  </div>
+                    <div className={styles.skill}>
+                        <h4>My skill</h4>
+                        <h5>discription skill</h5>
+                    </div>
 
 
-                  <div className={styles.skill}>
-                      <h4>my skill</h4>
-                      <h5>discription skill</h5>
-                  </div>
+                    <div className={styles.skill}>
+                        <h4>my skill</h4>
+                        <h5>discription skill</h5>
+                    </div>
 
-                  <div className={styles.skill}>
-                      <h4>my skill</h4>
-                      <h5>discription skill</h5>
-                  </div>
-              </div>
+                    <div className={styles.skill}>
+                        <h4>my skill</h4>
+                        <h5>discription skill</h5>
+                    </div>
+                </div>
 
-          </div>
+            </div>
 
-      </div>
-  );
+        </div>
+    );
 }
-
 
 
 export default Skills;
